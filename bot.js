5698e23fba1156de499541a2218fdff30dfaee31
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2018-11-25 14:16:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Welcome Bro ${client.user.tag}!`);
- 
-  });
-
 // Alpha Codes,// Alpha Codes,// Alpha Codes
 client.on('message', async message => {
     var command = message.content.toLowerCase().split(" ")[0];
